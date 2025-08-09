50febd9f45225803251feaceaaf7b6f1a6e9de31
Yanjun Tian <yanjuntian@microsoft.com>
2025-08-09 17:57:51 +0800

    fix the crawler, return more values of a recipe

--- allrecipes/__init__.py
+++ allrecipes/__init__.py
@@ -22,6 +22,8 @@ class AllRecipes(object):
 		url = base_url + query_url
 
 		req = urllib.request.Request(url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36')
+		req.add_header('Accept-Language', 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7')
 		req.add_header('Cookie', 'euConsent=true')
 
 		handler = urllib.request.HTTPSHandler(context=ssl._create_unverified_context())
@@ -32,7 +34,8 @@ class AllRecipes(object):
 		soup = BeautifulSoup(html_content, 'html.parser')
 
 		search_data = []
-		articles = soup.findAll("a", {"class": "mntl-card-list-items"})
+
+		articles = soup.findAll("a", {"class": "comp mntl-card-list-card--extendable mntl-universal-card mntl-document-card mntl-card card card--no-image"})
 
 		articles = [a for a in articles if a["href"].startswith("https://www.allrecipes.com/recipe/")]
 
@@ -67,24 +70,41 @@ class AllRecipes(object):
 		return search_data
 
 	@staticmethod
-	def _get_name(soup):
-		return soup.find("h1", {"id": "article-heading_2-0"}).get_text().strip(' \t\n\r')
+	def _get_title(soup):
+		return soup.find("h1", {"class": "article-heading"}).get_text().strip(' \t\n\r')
+
+	@staticmethod
+	def _get_summary(soup):
+		return soup.find("p", {"class": "article-subheading"}).get_text().strip(' \t\n\r')		
+
+	@staticmethod
+	def _get_picture_url(soup):
+		container = soup.find("div", class_="loc article-content")
+		if not container:
+			return ""
+
+		img_tag = container.find("img")
+		if img_tag and img_tag.has_attr("src") and img_tag["src"]:
+			return img_tag["src"]
+		elif img_tag and img_tag.has_attr("data-src") and img_tag["data-src"]:
+			return img_tag["data-src"]
+		return ""
 
 	@staticmethod
 	def _get_rating(soup):
-		return float(soup.find("div", {"id": "mntl-recipe-review-bar__rating_2-0"}).get_text().strip(' \t\n\r'))
+		return float(soup.find("div", {"id": "mm-recipes-review-bar__rating_1-0"}).get_text().strip(' \t\n\r'))
 
 	@staticmethod
 	def _get_ingredients(soup):
-		return [li.get_text().strip(' \t\n\r') for li in soup.find("div", {"id": "mntl-structured-ingredients_1-0"}).find_all("li")]
+		return [li.get_text().strip(' \t\n\r') for li in soup.find("div", {"id": "mm-recipes-structured-ingredients_1-0"}).find_all("li")]
 
 	@staticmethod
 	def _get_steps(soup):
-		return [li.get_text().strip(' \t\n\r') for li in soup.find("div", {"id": "recipe__steps_1-0"}).find_all("li")]
+		return [li.get_text().strip(' \t\n\r') for li in soup.find("div", {"id": "mm-recipes-steps_1-0"}).find_all("li")]
 
 	@staticmethod
 	def _get_times_data(soup, text):
-		return soup.find("div", {"id": "recipe-details_1-0"}).find("div", text=text).parent.find("div", {"class": "mntl-recipe-details__value"}).get_text().strip(' \t\n\r')
+		return soup.find("div", {"id": "mm-recipes-details_1-0"}).find("div", text=text).parent.find("div", {"class": "mm-recipes-details__value"}).get_text().strip(' \t\n\r')
 
 	@classmethod
 	def _get_prep_time(cls, soup):
@@ -99,7 +119,7 @@ class AllRecipes(object):
 		return cls._get_times_data(soup, "Total Time:")
 
 	@classmethod
-	def _get_nb_servings(cls, soup):
+	def _get_servings(cls, soup):
 		return cls._get_times_data(soup, "Servings:")
 
 	@classmethod
@@ -112,6 +132,8 @@ class AllRecipes(object):
 		# url = base_url + uri
 
 		req = urllib.request.Request(url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36')
+		req.add_header('Accept-Language', 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7')
 		req.add_header('Cookie', 'euConsent=true')
 
 		handler = urllib.request.HTTPSHandler(context=ssl._create_unverified_context())
@@ -122,14 +144,16 @@ class AllRecipes(object):
 		soup = BeautifulSoup(html_content, 'html.parser')
 
 		elements = [
-			{"name": "name", "default_value": ""},
+			{"name": "title", "default_value": ""},
+			{"name": "summary", "default_value": ""},
+			{"name": "picture_url", "default_value": ""},
 			{"name": "ingredients", "default_value": []},
 			{"name": "steps", "default_value": []},
 			{"name": "rating", "default_value": None},
 			{"name": "prep_time", "default_value": ""},
 			{"name": "cook_time", "default_value": ""},
 			{"name": "total_time", "default_value": ""},
-			{"name": "nb_servings", "default_value": ""},
+			{"name": "servings", "default_value": ""},
 		]
 
 		data = {"url": url}
